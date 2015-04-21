d00dde3fb5e3a9ab87e2c05047703cd3a3c9eb8b
Wendy Stewart <cellardoor@hotmail.ca>
2015-04-20 20:00:19 -0700

    Added destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,9 @@
 class ArticlesController < ApplicationController
+  def index
+	@articles = Article.all
+  end
 
- def index
-	 @articles = Article.all
- end
-
- def show
+  def show
    	@article = Article.find(params[:id])
  end
 
@@ -18,7 +17,7 @@ class ArticlesController < ApplicationController
  end
 
  def create
-   	@article = Article.new(article_params)
+   @article = Article.new(article_params)
  
   if @article.save
     redirect_to @article
@@ -35,6 +34,13 @@ end
   else
     render 'edit'
   end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
 end
 
 private
